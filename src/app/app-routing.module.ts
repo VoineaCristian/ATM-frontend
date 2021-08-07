121c991c19f9bdde538ea906238cf250b294eef7
VoineaCristian <voinea.silviucristian@gmail.com>
2021-08-07 18:01:09 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
+import { ListAccountsComponent } from './account/list-accounts/list-accounts.component';
+import {ListTransactionsComponent} from './transaction/list-transactions/list-transactions.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: 'accounts/:username', component: ListAccountsComponent,},
+  {path: 'transactions', component: ListTransactionsComponent}];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { AtmComponent } from './atm/atm.component';
+import { ListAccountsComponent } from './account/list-accounts/list-accounts.component';
+import {CommonModule} from "@angular/common"
+import { ListTransactionsComponent } from './transaction/list-transactions/list-transactions.component';
+import {RouterModule} from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AtmComponent,
+    ListAccountsComponent,
+    ListTransactionsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
